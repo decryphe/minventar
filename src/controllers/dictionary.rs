57c002d85a3acdacc54d7633e753e19a8aca51e4
decryphe <12104091+decryphe@users.noreply.github.com>
2026-02-04 09:37:03 +0100

    Output of: `cargo loco generate controller dictionary --html list remove update`

---
+++ src/controllers/dictionary.rs
@@ -0,0 +1,36 @@
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::unnecessary_struct_initialization)]
+#![allow(clippy::unused_async)]
+use loco_rs::prelude::*;
+
+#[debug_handler]
+pub async fn list(
+    ViewEngine(v): ViewEngine<TeraView>,
+    State(_ctx): State<AppContext>
+) -> Result<Response> {
+    format::render().view(&v, "dictionary/list.html", data!({}))
+}
+
+#[debug_handler]
+pub async fn remove(
+    ViewEngine(v): ViewEngine<TeraView>,
+    State(_ctx): State<AppContext>
+) -> Result<Response> {
+    format::render().view(&v, "dictionary/remove.html", data!({}))
+}
+
+#[debug_handler]
+pub async fn update(
+    ViewEngine(v): ViewEngine<TeraView>,
+    State(_ctx): State<AppContext>
+) -> Result<Response> {
+    format::render().view(&v, "dictionary/update.html", data!({}))
+}
+
+pub fn routes() -> Routes {
+    Routes::new()
+        .prefix("dictionaries/")
+        .add("list", get(list))
+        .add("remove", get(remove))
+        .add("update", get(update))
+}
